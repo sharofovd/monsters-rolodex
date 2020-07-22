abf77c4cfb533740f476bc0210b814587498b2d5
Dadaxon Sharofov <dadakhon.nuu@mail.ru>
2020-07-22 12:18:27 +0500

    Updates

---
+++ precache-manifest.c73ae7d11da7b89661fc99a211c6ec97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e477853734a705a058e9ffe0c7a03d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "335cbbf03cfb6cd76a98",
+    "url": "/monsters-rolodex/static/css/main.8441b49b.chunk.css"
+  },
+  {
+    "revision": "f757cf264434f883c100",
+    "url": "/monsters-rolodex/static/js/2.fcb612a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fcb612a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335cbbf03cfb6cd76a98",
+    "url": "/monsters-rolodex/static/js/main.253c1e2b.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
